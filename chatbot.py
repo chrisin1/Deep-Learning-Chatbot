8c6c192d16f6212e9a142b9cac523ac2bf44a872
Chris In <christopherin12@gmail.com>
2021-08-17 11:43:26 -0400

    init commit, testing nltk library

---
+++ chatbot.py
@@ -0,0 +1,44 @@
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import json
+import pickle
+
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+import random
+
+words=[]
+classes = []
+documents = []
+ignore_words = ['?', '!']
+data_file = open('job_intents.json', encoding='utf-8').read()
+intents = json.loads(data_file)
+
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+
+        w = nltk.word_tokenize(pattern)
+        words.extend(w)
+
+        documents.append((w, intent['tag']))
+
+
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+words = sorted(list(set(words)))
+
+classes = sorted(list(set(classes)))
+
+# print (len(documents), "documents")
+
+# print (len(classes), "classes", classes)
+
+# print (len(words), "unique lemmatized words", words)

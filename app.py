b2896dfd33a6bf6b060d9e2b085ddea4e22c648f
Chris In <christopherin12@gmail.com>
2021-08-17 20:20:13 -0400

    initialize flask app

---
+++ app.py
@@ -0,0 +1,22 @@
+from threading import local
+from flask import Flask, render_template, jsonify, request
+import processor
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret key'
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    return render_template('index.html', **locals())
+
+@app.route('/chatbot', methods=["GET", "POST"])
+def chatbotResponse():
+    if request.method == 'POST':
+        question = request.form['question']
+        response = processor.chatbot_response(question)
+
+    return jsonify({"response": response})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='8888', debug=True)
--- processor.py
+++ processor.py
@@ -37,7 +37,7 @@ def predict_class(sentence, model):
     p = bow(sentence, words, show_details=False)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
-    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    results = [[i,r] for i,r in enumerate(res) if r > ERROR_THRESHOLD]
     # sort by strength of probability
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
